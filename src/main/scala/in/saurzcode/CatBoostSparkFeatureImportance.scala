4f1ab9a7a2e3df9bc877680efd16d3a3810d7fac
saurzcode <7669344+saurzcode@users.noreply.github.com>
2021-05-02 19:24:11 +0530

    added feature importance

--- src/main/scala/in/saurzcode/CatBoostSparkFeatureImportance.scala
+++ src/main/scala/in/saurzcode/CatBoostSparkFeatureImportance.scala
@@ -1,70 +1,23 @@
 package in.saurzcode
-import ai.catboost.spark.{CatBoostClassificationModel, CatBoostClassifier, Pool}
-import org.apache.spark.ml.linalg.{SQLDataTypes, Vectors}
-import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
+
+import ai.catboost.spark.CatBoostClassificationModel
+import org.apache.spark.sql.SparkSession
+
 object CatBoostSparkFeatureImportance {
-  val spark = SparkSession.builder()
-    .master("local[*]")
-    .appName("ClassifierTest")
-    .getOrCreate();
 
-  val srcDataSchema = Seq(
-    StructField("features", SQLDataTypes.VectorType),
-    StructField("label", StringType)
-  )
-
-  val trainData = Seq(
-    Row(Vectors.dense(0.1, 0.2, 0.11), "0"),
-    Row(Vectors.dense(0.97, 0.82, 0.33), "1"),
-    Row(Vectors.dense(0.13, 0.22, 0.23), "1"),
-    Row(Vectors.dense(0.8, 0.62, 0.0), "0")
-  )
-
-  val trainDf = spark.createDataFrame(spark.sparkContext.parallelize(trainData), StructType(srcDataSchema))
-  val trainPool = new Pool(trainDf)
-
-  val evalData = Seq(
-    Row(Vectors.dense(0.22, 0.33, 0.9), "1"),
-    Row(Vectors.dense(0.11, 0.1, 0.21), "0"),
-    Row(Vectors.dense(0.77, 0.0, 0.0), "1")
-  )
-
-  val evalDf = spark.createDataFrame(spark.sparkContext.parallelize(evalData), StructType(srcDataSchema))
-  val evalPool = new Pool(evalDf)
-
-  val classifier = new CatBoostClassifier
-
-  // train model
-  val model = classifier.fit(trainPool, Array[Pool](evalPool))
-
-  // apply model
-  val predictions = model.transform(evalPool.data)
-  println("predictions")
-  predictions.show()
-
-  // save model
-  val savedModelPath = "/my_models/binclass_model"
-  model.write.save(savedModelPath)
-
-  // save model as local file in CatBoost native format
-  val savedNativeModelPath = "./my_local_models/binclass_model.cbm"
-  model.saveNativeModel(savedNativeModelPath)
-
-  // load model (can be used in a different Spark session)
-
-  val loadedModel = CatBoostClassificationModel.load(savedModelPath)
-
-  val predictionsFromLoadedModel = loadedModel.transform(evalPool.data)
-  println("predictionsFromLoadedModel")
-  predictionsFromLoadedModel.show()
+  def main(args: Array[String]): Unit = {
 
 
-  // load model as local file in CatBoost native format
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .appName("FeatureImportanceTest")
+      .getOrCreate();
 
-  val loadedNativeModel = CatBoostClassificationModel.loadNativeModel(savedNativeModelPath)
+    val loadedModel = CatBoostClassificationModel.loadNativeModel("models/binclass_model.cbm")
 
-  val predictionsFromLoadedNativeModel = loadedNativeModel.transform(evalPool.data)
-  println("predictionsFromLoadedNativeModel")
-  predictionsFromLoadedNativeModel.show()
+    val featureImportance = loadedModel.getFeatureImportancePrettified()
+
+    featureImportance.foreach(fi => println("[" + fi.featureName + "," + fi.importance + "]"))
+
+  }
 }
